17a646e97b885d447b310c47bbc48a442f8a0cba
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-01 18:38:48 +0530

    Login page created

--- atm_machine.cpp
+++ atm_machine.cpp
@@ -1,34 +1,50 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void showMenu(){
-    cout<<"********************Welcome to the bank of Chitkara********************"<<endl<<endl;
-    cout<<"**********Menu**********"<<endl;
-    cout<<"1. Check Balance"<<endl;
-    cout<<"2. Deposit Money"<<endl;
-    cout<<"3. Withdraw Money"<<endl;
-    cout<<endl;
-    cout<<"***********************************************************************"<<endl<<endl;
+void showMenu()
+{
+    cout << "********************Welcome to the bank of Chitkara********************" << endl
+         << endl;
+    cout << "**********Menu**********" << endl;
+    cout << "1. Check Balance" << endl;
+    cout << "2. Deposit Money" << endl;
+    cout << "3. Withdraw Money" << endl;
+    cout << endl;
+    cout << "***********************************************************************" << endl
+         << endl;
 }
 
 int main()
 {
     showMenu();
-    //Storing Account Credentials
-    int accNo, pass, option;
+    // Storing Account Credentials
+    int accNo, pass, option, index;
     int accArr[10] = {4324, 1478, 4814, 7014, 3895, 5092, 7162, 4672, 6158, 1206};
     string nameArr[10] = {"Abhishek", "Abhay", "Sujal", "Kanika", "Mohak", "Sidhi", "Abhiraj", "Meher", "Nidhi", "Harsh"};
     int passArr[10] = {4234, 8741, 4184, 4107, 5983, 2905, 2617, 2764, 8516, 6021};
     int BalanceArr[10] = {73720, 41653, 39568, 77508, 80199, 25312, 56400, 76229, 30718, 40398};
 
-    //Taking input for Account credentials
-    cout<<"Account Number: ";
-    cin>>accNo;
-    cout<<endl;
-    cout<<"ATM Pin: ";
-    cin>>pass;
-
-
-
+    // Taking input for Account credentials
+    cout << "Account Number: ";
+    cin >> accNo;
+    cout << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        if (accNo = accArr[i])
+        {
+            cout<<"ATM Pin: ";
+            cin>>pass;
+            if(pass == passArr[i]){
+                cout<<"What you want to do select option from menu: ";
+            }
+            else{
+                cout<<"Incorrect Pin"<<endl;
+            }
+        }
+        else
+        {
+            cout<<"Incorrect Data"<<endl;
+        }
+    }
     return 0;
 }
