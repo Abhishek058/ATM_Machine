8d888d423a6219494093495cb1f6ff4c04c87a3b
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-01 14:46:43 +0530

    menu function added

---
+++ atm_machine.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void showMenu(){
+    cout<<"********************Welcome to the bank of Chitkara********************"<<endl<<endl;
+    cout<<"**********Menu**********"<<endl;
+    cout<<"1. Check Balance"<<endl;
+    cout<<"2. Deposit Money"<<endl;
+    cout<<"3. Withdraw Money"<<endl;
+    cout<<"***********************************************************************"<<endl;
+}
+
+int main()
+{
+    
+    return 0;
+}
